53d9bb23e0943093e21777a9e767d619e64a5158
Ben Tidy <ogftpi@msn.com>
2023-08-06 18:29:58 +0100

    [update] Force validation exceptions to display in json

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +27,22 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ValidationException) {
+            return $this->handleValidationException($request, $exception);
+        }
+
+        return parent::render($request, $exception);
+    }
+
+    public function handleValidationException($request, ValidationException $exception)
+    {
+        return response()->json([
+            'message' => 'Validation failed',
+            'errors' => $exception->validator->getMessageBag(),
+        ], 422);
     }
 }
